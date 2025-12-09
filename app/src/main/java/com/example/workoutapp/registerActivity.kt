4834512fd203c7bf89229109899c784572913842
Scott Horabin <126489544+scotthorabin@users.noreply.github.com>
2025-12-09 22:59:36 +0000

    Changed UI for Register and Login Pages

--- app/src/main/java/com/example/workoutapp/registerActivity.kt
+++ app/src/main/java/com/example/workoutapp/registerActivity.kt
@@ -4,16 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.workoutapp.databinding.ActivityRegisterBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
     private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +30,19 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+
+        // This is the google sign in option config
+
+        val signIn = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, signIn)
+
+        binding.btnGoogle.setOnClickListener {
+            signInWithGoogle()
+        }
 
         binding.btnRegister.setOnClickListener {
             val email = binding.email.text.toString()
@@ -58,5 +79,52 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, loginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private val googleSignInLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    this,
+                    "Google sign-in failed, please try again later",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        googleSignInLauncher.launch(signInIntent)
+    }
+
+    private fun newUser() {
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credentials = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credentials)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    runOnUiThread {
+                        val isNewUser = task.result?.additionalUserInfo?.isNewUser ?: false
+                        Toast.makeText(
+                            this,
+                            if (isNewUser) "Welcome new user!" else "Welcome back!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        startActivity(Intent(this, DashboardActivity::class.java))
+                        finish()
+                    }
+                } else {
+                    Toast.makeText(this, "Unable to sign in: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 }

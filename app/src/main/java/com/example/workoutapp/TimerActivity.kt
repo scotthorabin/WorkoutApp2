f92e8ac9d7375b5b9ce4191330dd6abea55257bb
Scott Horabin <126489544+scotthorabin@users.noreply.github.com>
2025-12-13 17:57:57 +0000

    Created timer for specific exercises, fixed UI and changes to Login, Dashboard, and Timer Activities (13/12/2025)

--- app/src/main/java/com/example/workoutapp/TimerActivity.kt
+++ app/src/main/java/com/example/workoutapp/TimerActivity.kt
@@ -1,20 +1,185 @@
 package com.example.workoutapp
 
+import android.app.Dialog
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+
+
+
 
 class TimerActivity : AppCompatActivity() {
+
+
+    //Variables
+    private var selectedTime: Int = 0
+    private var countdownTimer: CountDownTimer? = null
+    private var progressT: Int = 0
+    private var pauseOffSet: Long = 0
+    private var start: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_timer)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // When clicked, goes to set time function
+        val addTimeBtn = findViewById<ImageButton>(R.id.addBtn)
+
+        addTimeBtn.setOnClickListener {
+            setTime()
+        }
+
+        // When clicked on, goes to timerSetup function
+        val startBtn = findViewById<Button>(R.id.btnPlayPause)
+
+        startBtn.setOnClickListener {
+            timerSetup()
+        }
+
+        // When clicked on, goes to ResetTime function
+        val resetBtn = findViewById<ImageButton>(R.id.resetBtn)
+        resetBtn.setOnClickListener {
+            resetTime()
+        }
+
+        // When clicked on, goes to addExtraTime function
+        val addTime = findViewById<TextView>(R.id.SecondsP)
+        addTime.setOnClickListener {
+            addExtraTime()
         }
     }
+
+
+    // Function that Set ups timer
+    private fun timerSetup(){
+    val startBtn = findViewById<Button>(R.id.btnPlayPause)
+        if(selectedTime > progressT)
+        {
+            if (start)
+            {
+                startBtn.text = "Pause"
+                startTime(pauseOffSet)
+                start = false
+            }
+            else
+            {
+                start = true
+                startBtn.text = "Resume"
+                timePause()
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "Please enter a time", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // Function that pauses Timer
+    private fun timePause() {
+        if (countdownTimer!= null)
+        {
+            countdownTimer!!.cancel()
+        }
+    }
+
+
+    // Function that starts timer
+    private fun startTime(pauseOffSetL: Long)
+    {
+        val progress = findViewById<ProgressBar>(R.id.pbTimer)
+        progress.progress = progressT
+        countdownTimer = object : CountDownTimer(
+            (selectedTime*1000).toLong() - pauseOffSetL * 1000, 1000
+        ) {
+            override fun onFinish() {
+                Toast.makeText(this@TimerActivity, "Time is Up!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTick(p0: Long) {
+                progressT++
+                pauseOffSet = selectedTime.toLong() - p0/1000
+                progress.progress = selectedTime - progressT
+                val timeLeft = findViewById<TextView>(R.id.TimeLeft)
+                timeLeft.text =(selectedTime - progressT).toString()
+            }
+
+        }.start()
+    }
+
+
+    // Function for when User sets amount of time, changes progress bar
+    private fun setTime() {
+
+        val timeDialog = Dialog(this)
+        timeDialog.setContentView(R.layout.add_timer)
+        val setTime = timeDialog.findViewById<EditText>(R.id.getUserTime)
+        val timeLeft = findViewById<TextView>(R.id.TimeLeft)
+        val startBtn = findViewById<Button>(R.id.btnPlayPause)
+        val progress = findViewById<ProgressBar>(R.id.pbTimer)
+        timeDialog.findViewById<Button>(R.id.ConfirmBtn).setOnClickListener {
+
+            // If time is empty, then message appears, else get time and apply to XML
+            if (setTime.text.isEmpty())
+            {
+                Toast.makeText(this, "Please enter a time duration", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                timeLeft.text = setTime.text
+                startBtn.text = "Start"
+                selectedTime = setTime.text.toString().toInt()
+                progress.max = selectedTime
+            }
+            timeDialog.dismiss()
+        }
+        timeDialog.show()
+
+    }
+
+    // Function that resets time
+    private fun resetTime() {
+        countdownTimer!!.cancel()
+        progressT = 0
+        selectedTime = 0
+        pauseOffSet = 0
+        countdownTimer = null
+        val startBtn = findViewById<Button>(R.id.btnPlayPause)
+        startBtn.text = "Start"
+        start = true
+        val progressBar = findViewById<ProgressBar>(R.id.pbTimer)
+        progressBar.progress = 0
+        val timeLeft = findViewById<TextView>(R.id.TimeLeft)
+        timeLeft.text = "0"
+    }
+
+    // Function that adds 10 seconds on when user clicks TextView
+    private fun addExtraTime() {
+        val progressBar = findViewById<ProgressBar>(R.id.pbTimer)
+        if (selectedTime!=0)
+        {
+            selectedTime += 10
+            progressBar.max = selectedTime
+            timePause()
+            startTime(pauseOffSet)
+            Toast.makeText(this, "10 seconds added on", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // Function that removes Timer countdown
+    override fun onDestroy() {
+        super.onDestroy()
+        if(countdownTimer!= null)
+            countdownTimer?.cancel()
+        progressT = 0
+    }
+
+
 }
--- app/src/main/java/com/example/workoutapp/loginActivity.kt
+++ app/src/main/java/com/example/workoutapp/loginActivity.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.workoutapp.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class loginActivity : AppCompatActivity() {
 
@@ -16,6 +22,7 @@ class loginActivity : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
 
+    private lateinit var googleSignInClient: GoogleSignInClient
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,9 +39,9 @@ class loginActivity : AppCompatActivity() {
             val password = binding.password.text.toString()
 
             // if email and password is not empty, then use database to check if account is valid
-            if(email.isNotEmpty() && password.isNotEmpty()) {
+            if (email.isNotEmpty() && password.isNotEmpty()) {
                 firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
-                    if (it.isSuccessful){
+                    if (it.isSuccessful) {
                         // If successful, load dashboard page
                         val intent = Intent(this, DashboardActivity::class.java)
                         startActivity(intent)
@@ -53,5 +60,64 @@ class loginActivity : AppCompatActivity() {
             val intent = Intent(this, forgotPasswordActivity::class.java)
             startActivity(intent)
         }
+        // This is the google sign in option config
+        // From Google Documentation
+
+        val signIn = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, signIn)
+
+        binding.btnGoogle.setOnClickListener {
+            signInWithGoogle()
         }
     }
+    // Google sign in attempt
+    private val googleSignInLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    this,
+                    "Google sign-in failed, please try again later",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        googleSignInLauncher.launch(signInIntent)
+    }
+
+
+    // When user logs in with google, either a welcome back or welcome message appears, if not, exception
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credentials = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credentials)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    runOnUiThread {
+                        val isNewUser = task.result?.additionalUserInfo?.isNewUser ?: false
+                        Toast.makeText(
+                            this,
+                            if (isNewUser) "Welcome new user!" else "Welcome back!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        startActivity(Intent(this, DashboardActivity::class.java))
+                        finish()
+                    }
+                } else {
+                    Toast.makeText(this, "Unable to sign in: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+}
